59532885596c94fe5ef095ab39ac7f04e28187ae
Samuel Yepes <samuelyepes1122020@correo.itm.edu.co>
2026-09-18 07:45:32 -0500

    "Add generic repository, match repository and database seeder (Module 3)"

---
+++ GolBet.Repositories/Interfaces/IMatchRepository.cs
@@ -0,0 +1,11 @@
+﻿// Interfaces/IMatchRepository.cs
+using GolBet.Entities;
+using GolBet.Entities.Enums;
+
+namespace GolBet.Repositories.Interfaces;
+
+public interface IMatchRepository : IGenericRepository<Match>
+{
+    Task<IEnumerable<Match>> GetAllWithTeamsAsync(MatchStatus? status = null);
+    Task<Match?> GetByIdWithDetailsAsync(int id);
+}
--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,5 +1,6 @@
 using GolBet.Repositories.Data;
-
+using GolBet.Repositories.Implementations;
+using GolBet.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -18,7 +19,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await DbSeeder.SeedAsync(context);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
